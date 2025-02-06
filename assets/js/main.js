e5c6410fdd221d408a04d322c3897e528129110b
Luke Welsh <21pinball21@protonmail.com>
2025-02-06 13:25:34 -0600

    (Test 2) Restrict EXIF data for screens 1024px and larger

--- assets/js/main.js
+++ assets/js/main.js
@@ -269,12 +269,12 @@
                 var $image_img = $a.children('img');
                 var data = exifDatas[$image_img.data('name')];
                 if (data === undefined) {
-                    // EXIF data					
+                    // EXIF data
                     EXIF.getData($image_img[0], function () {
                         data = exifDatas[$image_img.data('name')] = getExifDataMarkup(this);
                     });
                 }
-                return data !== undefined ? '<p>' + data + '</p>' : ' ';
+                return window.isWideScreen && data !== undefined ? '<p>' + data + '</p>' : ' ';
             },
             fadeSpeed: 300,
             onPopupClose: function () {
